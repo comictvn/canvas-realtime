34685275e56d74deedec44cfe3d1215c39fb6ae4
Duy Nghia <comictvn@gmail.com>
2014-08-26 18:56:34 +0700

    Test

---
+++ server.js
@@ -0,0 +1,38 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+// Broadcast all draw clicks.
+io.on('connection', function(socket){ 
+     
+    socket.on('message',function(msg){
+    	console.log(msg);
+        socket.broadcast.emit('message', {n:msg});
+     });
+ 
+ 
+    socket.on('disconnect',function(){
+        console.log('Server has disconnected');
+    });
+ 
+    socket.on('iran', function(data){
+      console.log("**************" + data.todo + "******************");
+    });
+     
+});
+
+// Send client html.
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/client.html')
+})
+
+app.get('/client', function(req, res) {
+    res.sendfile(__dirname + '/listent.html')
+})
+
+
+
+http.listen(3000, function() {
+	console.log('listening on *: 3000');
+})
